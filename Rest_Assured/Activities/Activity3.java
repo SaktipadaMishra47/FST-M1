e6c9f07e79286464398f102215c843148dd022b3
SaktipadaMishra47 <57944498+SaktipadaMishra47@users.noreply.github.com>
2025-02-17 21:44:05 +0530

    Add files via upload

--- Rest_Assured/Activities/Activity3.java
+++ Rest_Assured/Activities/Activity3.java
@@ -1,66 +1,98 @@
-package fst.Restassured;
+package activities;
 
-import static org.testng.Assert.assertEquals;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
 
-import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import io.restassured.*;
-import io.restassured.RestAssured.*;
-import io.restassured.path.json.JsonPath;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import io.restassured.response.ResponseBody;
-import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 
 public class Activity3 {
+	String baseUri = "https://petstore.swagger.io/v2/pet";
+	RequestSpecification requestSpec;
+	ResponseSpecification responseSpec;
 
-	String baseURL = "https://petstore.swagger.io/v2/pet";
-	Response response;
-	int petId = 0;
+	@BeforeClass
+	public void setup() {
+		requestSpec = new RequestSpecBuilder()
+				.setContentType(ContentType.JSON)
+				.setBaseUri(baseUri)
+				.build();
 
-	@Test(priority = 1)
-	public void createNewPet() {
-		System.out.println("*****************Create Starts **********************");
-		
-		response = RestAssured.given().baseUri(baseURL).log().all().contentType("application/json").body("""
-								{
-				  "id": 77232,
-				  "name": "Riley",
-				  "status": "alive"
-				}
-								""").post().then().log().all().extract().response();
-		JsonPath jsonResponse = response.jsonPath();
-		
-		petId = jsonResponse.getInt("id");
-		
-		System.out.println("petId is " +petId);
-		Assert.assertEquals(petId, 77232);
-		Assert.assertEquals(jsonResponse.getString("name"), "Riley");
-		Assert.assertEquals(jsonResponse.getString("status"), "alive");
-
-	}
-	@Test(priority=2)
-	public void getPet() {
-		System.out.println("*****************get Starts **********************");
-		
-		response = RestAssured.given().baseUri(baseURL).log().all().contentType("application/json").
-				get("/"+petId).then().log().all().extract().response();
-		JsonPath jsonResponse = response.jsonPath();
-		petId = jsonResponse.getInt("id");
-		Assert.assertEquals(petId, 77232);
-		Assert.assertEquals(jsonResponse.getString("name"), "Riley");
-		Assert.assertEquals(jsonResponse.getString("status"), "alive");
-
+		responseSpec = new ResponseSpecBuilder()
+				.expectContentType(ContentType.JSON)
+				.expectStatusCode(200)
+				.expectBody("status", equalTo("alive"))
+				.build();
 	}
 	
-	@Test(priority =3)
-	public void deletePet() {
-		System.out.println("*****************delete Starts **********************");
+	@DataProvider(name = "petInfo")
+	public Object[][] dataProvider(){
+		Object[][] testData = new Object[][] {
+			{ 77232, "Riley", "alive" }, 
+			{ 77233, "Hansel", "alive" } 
+		};		
+		return testData;
+	}
+
+	@Test(priority = 1)
+	public void postRequestTest() {
+		String pet1Body = """
+				{
+				"id": 77232,
+				"name": "Riley",
+				"status": "alive"
+				}
+				""";
+
+		String pet2Body = """
+				{
+				"id": 77233,
+				"name": "Hansel",
+				"status": "alive"
+				}
+				""";
+
+		Response pet1Response = 
+				given().spec(requestSpec).body(pet1Body)
+				.when().post();
+
+		Response pet2Response = 
+				given().spec(requestSpec).body(pet2Body)
+				.when().post();
+
+		pet1Response.then().spec(responseSpec);
+		pet2Response.then().spec(responseSpec);
+	}
+
+	@Test(dataProvider = "petInfo", priority = 2)
+	public void getRequestTest(int petId, String name, String status) {		
+		Response getResponse = given()
+				.spec(requestSpec)
+				.when().pathParam("petId", petId)
+				.get("/{petId}");
+
+		getResponse.then().spec(responseSpec);
 		
-		response = RestAssured.given().baseUri(baseURL).log().all().contentType("application/json").
-				delete("/"+petId).then().log().all().extract().response();
-		
-		Assert.assertEquals(response.getStatusCode() ,200 );
-		Assert.assertEquals(petId,77232,"Unable to delte the code");	
-	}	
+		getResponse.then().body("id", equalTo(petId));
+		getResponse.then().body("name", equalTo(name));
+	}
+
+	@Test(dataProvider = "petInfo", priority = 3)
+	public void deleteRequestTest(int petId, String name, String status) {		
+		Response deleteResponse = given()
+				.spec(requestSpec)
+				.when().pathParam("petId",petId)
+				.delete("/{petId}");
+
+		deleteResponse.then().statusCode(200);
+		deleteResponse.then().body("message",equalTo(petId+""));
+	}
 }
