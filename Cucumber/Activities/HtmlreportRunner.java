c26a81ff2a4da96695cec63053cce786a5345ae6
SaktipadaMishra47 <57944498+SaktipadaMishra47@users.noreply.github.com>
2025-02-25 16:45:59 +0530

    Add files via upload

---
+++ Cucumber/Activities/HtmlreportRunner.java
@@ -0,0 +1,19 @@
+package runners;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "Features",
+    glue = {"stepDefinitions"},
+    tags = "@SimpleAlert",
+    plugin = {"html: test-reports"},
+    monochrome = true
+)
+
+public class HtmlreportRunner {
+
+}
